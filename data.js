9a692814497a6ab3d0d016d6e3f5bdfb0dabd296
7-langd-roid <anikitsomadder7@gmail.com>
2026-06-13 03:30:46 -0400

    Update applyLinks function URLs
    Removed query parameter from applyLinks function URLs.

--- data.js
+++ data.js
@@ -24,10 +24,9 @@ const ICONS = {
 function applyLinks(query) {
   const q = encodeURIComponent(query);
   return {
-    uk: `https://www.af9m8trk.com/85N11AS3/QLQ11Y4B/?s1=Anik {q}`,
-    us: `https://www.af9m8trk.com/85N11AS3/QLQ11Y4B/?s1=Anik {q}`,
-    ca: `https://www.af9m8trk.com/85N11AS3/QLQ11Y4B/?s1=Anik {q}`,
-  };
+    uk: `https://www.af9m8trk.com/85N11AS3/QLQ11Y4B/?s1=Anik `,
+    us: `https://www.af9m8trk.com/85N11AS3/QLQ11Y4B/?s1=Anik `,
+    ca: `https://www.af9m8trk.com/85N11AS3/QLQ11Y4B/?s1=Anik `,
 }
 
 const JOB_CATEGORIES = [
